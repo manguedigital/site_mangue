159e91e53568bc12d5b74ac40576248987d76d5f
Hugo Sousa <goj1@hotmail.com>
2021-08-02 23:18:34 -0300

    Arquitetura do site/app
    Criação da arquitetura do site/app.

---
+++ android/app/src/main/kotlin/com/example/mangue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mangue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            Column(
+              children: [
+                Center(
+                  child: Text('Lado Esquerdo'),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Center(
+                  child: Text('Lado Direito'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
